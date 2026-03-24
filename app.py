a553e19b2ed0cdb9b4b51512b6449afba2d4cf6d
Brian Lynch <bklynch.connect@gmail.com>
2026-03-24 12:36:23 -0400

    added autorefresh

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
+from streamlit_autorefresh import st_autorefresh
+
+st_autorefresh(interval=120000, key="datarefresh")
 
 # --- CONFIG ---
 SHEET_NAME = "wos_crazyjoe_signup"
